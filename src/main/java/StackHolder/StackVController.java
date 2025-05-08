cdb9504ba33a9c5db7b35120e40610bc8e0f984d
Rafail Lishman <rafaillishman@yahoo.com>
2025-05-08 12:03:28 -0400

    Added some comments and got rid of useless code.

--- src/main/java/StackHolder/StackVController.java
+++ src/main/java/StackHolder/StackVController.java
@@ -5,8 +5,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.util.ArrayList;
-
 public class StackVController {
     String all_words ="";
     StackRL<String> undo = new StackRL<>();
@@ -18,11 +16,7 @@ public class StackVController {
     private TextField addANewWord;
 
     @FXML
-    private Button addWord;
-    @FXML
-    private Button undoButton;
-    @FXML
-    private Button redoButton;
+    private Button addWord, undoButton, redoButton;
 
     @FXML
     protected void onAddWordButtonClick() {
@@ -30,22 +24,22 @@ public class StackVController {
         undoTop.setText(undo.peek());
         undoButton.setDisable(false);
 
-        while(!redo.isEmpty()){
+        while(!redo.isEmpty()){ // O(n) remover
             redo.pop();
         }
         redoButton.setDisable(true);
-
         redoTop.setText("");
-        docText.setText("");
-        all_words += undo.peek() + " ";
+        // disable Redo button because there isn't anything to Redo.
+
+        all_words += undo.peek() + " "; // uses the top of the stack and adds it to the String
         docText.setText(all_words);
-        addANewWord.clear();
+        addANewWord.clear(); // clear textfield
     }
 
     @FXML
     protected void onUndoButtonClick(){
         String undidWord = undo.pop();
-        if(undo.isEmpty()){
+        if(undo.isEmpty()){ // Disable the Undo button because there isn't anything in the Undo stack.
             undoTop.setText("");
             undoButton.setDisable(true);
         } else{
@@ -54,14 +48,14 @@ public class StackVController {
         }
 
         redo.push(undidWord);
-        if(redo.isEmpty()){
+        if(redo.isEmpty()){ //Here just in case the program SOMEHOW breaks, haven't had an issue.
             redoTop.setText("");
             redoButton.setDisable(true);
         }else{
             redoTop.setText(redo.peek());
             redoButton.setDisable(false);
         }
-
+        //odd math, but basically it just takes the word that we popped out and then subtracts the total String length with it +1 because I added " "
         all_words = all_words.substring(0, (all_words.length() - (undidWord.length()+1)));
         docText.setText(all_words);
     }
@@ -70,7 +64,7 @@ public class StackVController {
     protected void onRedoButtonClick(){
         String redidWord = redo.pop();
 
-        if(redo.isEmpty()){
+        if(redo.isEmpty()){ // Null checker, if the Redo stack is Empty it will disable the Redo button.
             redoTop.setText("");
             redoButton.setDisable(true);
         } else{
